566f2da142e5fdb791471f77c13317a53035cbd3
Anthony Collier <hello@acollier.dev>
2022-10-02 18:18:04 -0400

    :sparkles: Properly display generated image via embed

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,8 +3,11 @@ use std::collections::HashMap;
 use std::convert::Infallible;
 use std::time::Instant;
 use actix_web::rt::time::interval;
+use rusty_interaction::Builder;
 use rusty_interaction::handler::InteractionHandler;
 use rusty_interaction::types::application::ApplicationCommandInteractionData;
+use rusty_interaction::types::components::{ComponentButtonBuilder, ComponentButtonStyle, ComponentRowBuilder};
+use rusty_interaction::types::embed::{Embed, EmbedBuilder, EmbedFooter, EmbedImage};
 use rusty_interaction::types::interaction::*;
 use rusty_interaction::types::Snowflake;
 use rusty_interaction::types::message::Message;
@@ -12,6 +15,14 @@ use image;
 
 const PUB_KEY: &str = "d60cfa59f0d6b0ed79b2437ec22b3f2edd7b5414e59ae1578dd1211a1ff098b3";
 const APP_ID: u64 = 937152370331242557;
+
+// We defer in this instance, because we don't want to edit anything
+#[rusty_interaction::component_handler]
+#[defer]
+async fn delete_button(ctx: Context) -> Result<InteractionResponse, Infallible> {
+    if let Ok(_) = ctx.delete_original().await {}
+    return ctx.respond().none();
+}
 
 #[rusty_interaction::slash_command]
 async fn ping_handler(ctx: Context) -> Result<InteractionResponse, Infallible> {
@@ -32,15 +43,30 @@ async fn quote_handler(ctx: Context) -> Result<InteractionResponse, Infallible> 
 
     println!("generating image...?");
     let before_call = Instant::now();
-    image::generate_image(message_content).await;
+    let url = image::generate_image(message_content).await;
     println!("total time elapsed: {:?}", before_call.elapsed().as_millis());
+    println!("url: {:?}", url);
 
-    ctx.respond().content(
-        format!(
-            "here's the message: `{}`",
-            message_content
+    let components = ComponentRowBuilder::default()
+        .add_button(
+            ComponentButtonBuilder::default()
+                .label("Delete")
+                .custom_id("DELETE")
+                .style(&ComponentButtonStyle::Danger)
+                .finish(),
         )
-    ).finish()
+        .finish();
+
+    let embed = EmbedBuilder::default()
+        .description(format!("here's the message: `{}`", message_content))
+        .color(0xc9a0dc as u32)
+        .image(EmbedImage::default().url(url))
+        .finish();
+
+    ctx.respond()
+        .add_component_row(components)
+        .add_embed(&embed)
+        .finish()
 }
 
 #[actix_web::main]
@@ -51,6 +77,9 @@ async fn main() {
 
     handle.add_global_command("ping", ping_handler);
     handle.add_global_command("quote", quote_handler);
+
+    // Here we attach our custom ids we defined with our buttons to the handler
+    handle.add_component_handle("DELETE", delete_button);
 
     handle.run(3000).await;
 }
